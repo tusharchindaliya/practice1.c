10814a253f974f49826818eed72e581b718e4853
tusharchindaliya <tusharchindaliya51@gmail.com>
2025-08-01 19:06:45 +0530

    prac

--- practice1.c
+++ practice1.c
@@ -1,9 +1,20 @@
 #include<stdio.h>
-int main()
-{
-    int l=20;
-    int b=30;
-    int a=l*b;
-    printf("%d ",a);
+
+int main() {
+    float length, width, area;
+
+    // Get input from the user
+    printf("Enter the length of the rectangle: ");
+    scanf("%f", &length);
+
+    printf("Enter the width of the rectangle: ");
+    scanf("%f", &width);
+
+    // Calculate the area
+    area = length * width;
+
+    // Display the result
+    printf("The area of the rectangle is: %.2f\n", area);
+
     return 0;
 }
